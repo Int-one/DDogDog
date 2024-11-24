c4e06071e899b6ed53099243e11be93d97a5b3fa
Eric4848 <hajungsu88@gmail.com>
2024-11-24 15:12:36 +0900

    [Feat]Trade페이지 구현에 필요한 API

--- BackEnd/DDogDog/src/main/java/com/ddogdog/model/dao/TradeDao.java
+++ BackEnd/DDogDog/src/main/java/com/ddogdog/model/dao/TradeDao.java
@@ -1,12 +1,20 @@
 package com.ddogdog.model.dao;
 
+import com.ddogdog.model.dto.Pet;
 import com.ddogdog.model.dto.Trade;
 import java.util.List;
+import java.util.Map;
 
 public interface TradeDao {
     int insertTrade(Trade trade);
 
     List<Trade> getAllTrades();
+    
+    List<Trade> getAllWillTrades();
+    
+    List<Pet> getPetsByTradeId(Long tradeId);
+    
+    List<Trade> getTradesByCandId(String candId);
 
     Trade getTradeById(Long tradeId);
     
@@ -15,4 +23,6 @@ public interface TradeDao {
     int updateTrade(Trade trade);
 
     int deleteTrade(Long tradeId);
+    
+    List<Map<String, Object>> getAllPleaseTrades();
 }
--- BackEnd/DDogDog/src/main/java/com/ddogdog/service/TradeService.java
+++ BackEnd/DDogDog/src/main/java/com/ddogdog/service/TradeService.java
@@ -1,12 +1,20 @@
 package com.ddogdog.service;
 
+import com.ddogdog.model.dto.Pet;
 import com.ddogdog.model.dto.Trade;
 import java.util.List;
+import java.util.Map;
 
 public interface TradeService {
     boolean createTrade(Trade trade);
 
     List<Trade> getAllTrades();
+
+    List<Trade> getAllWillTrades();
+    
+    List<Pet> getPetsByTradeId(Long tradeId);
+    
+    List<Trade> getTradesByCandId(String candId);
 
     Trade getTradeById(Long tradeId);
     
@@ -15,4 +23,6 @@ public interface TradeService {
     boolean updateTrade(Trade trade);
 
     boolean deleteTrade(Long tradeId);
+    
+    public List<Map<String, Object>> getAllPleaseTrades();
 }
